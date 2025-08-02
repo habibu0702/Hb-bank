e173ca84834c2a091b1b51db682f0c5d8e12b9e4
TSB <habibusulaiman287@gmail.com>
2025-08-02 04:52:48 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
+    ssl: { rejectUnauthorized: false},
     port: 5432
 });
 
